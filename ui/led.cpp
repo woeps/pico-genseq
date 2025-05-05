0b68649cfb89212b72a1f97a3cef3461c2dfcfa0
Christoph Porod-Wanasek <christoph@porod-wanasek.at>
2025-05-05 12:47:20 +0200

    remove logging from Led

--- ui/led.cpp
+++ ui/led.cpp
@@ -8,28 +8,24 @@ Led::Led(uint8_t pin)
     : pin(pin), state(false), blinking(false), onTime(0), offTime(0), lastToggleTime(0) {}
 
 void Led::init() {
-    printf("Led init\n");
     gpio_init(pin);
     gpio_set_dir(pin, GPIO_OUT);
     off();
 }
 
 void Led::on() {
-    printf("Led on\n");
     gpio_put(pin, 1);
     state = true;
     blinking = false;
 }
 
 void Led::off() {
-    printf("Led off\n");
     gpio_put(pin, 0);
     state = false;
     blinking = false;
 }
 
 void Led::toggle() {
-    printf("Led toggle\n");
     state = !state;
     gpio_put(pin, state);
 }
@@ -44,19 +40,16 @@ void Led::toggle() {
  * @param offTime The time in milliseconds the LED should be off.
  */
 void Led::blink(uint32_t onTime, uint32_t offTime) {
-    printf("Led blink\n");
     this->onTime = onTime;
     this->offTime = offTime;
     this->blinking = true;
     lastToggleTime = to_ms_since_boot(get_absolute_time());
-    //on(); // Start in the on state
     gpio_put(pin, 1);
     this->state = true;
 }
 
 void Led::update() {
     if (!blinking) return;
-    printf("Led update\n");
     
     uint32_t currentTime = to_ms_since_boot(get_absolute_time());
     uint32_t elapsed = currentTime - lastToggleTime;
